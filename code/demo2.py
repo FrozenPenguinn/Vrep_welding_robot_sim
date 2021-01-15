158fb01aa38e4902c66934d132bdab2e9cae8f00
FrozenPenguinn <Zhouyh.henry2016@outlook.com>
2021-01-15 17:26:19 +0800

    update on forward kinematics

--- code/demo2.py
+++ code/demo2.py
@@ -2,26 +2,48 @@
 import vrep
 import time
 import numpy as np
+import numpy.matlib
 import math
 from connection import *
 from helper_functions import *
 #from demo1 import Move_to_joint_position
 
 # initialize
-global clientID
 clientID = 0
 joint_handle = np.zeros(6, dtype=np.int)
 joint_angle = np.zeros(6)
-pos_or_mat = np.zeros((3,4))
+pos_or_mat = np.zeros(12)
+euler = np.zeros(3)
 
 # connect and get handles
 clientID, joint_handle = Connect()
 
 # initialize UR5 parameters
-d = np.array([89.2,0,0,109.15,94.65,82.3])
-a = np.array([0,-425,-392.25,0,0,0])
+d = np.array([0.0892,0,0,0.10915,0.09465,0.0823])
+a = np.array([0,-0.425,-0.39225,0,0,0])
 alpha = np.array([PI/2,0,0,PI/2,-PI/2,0])
 
+def show_dummy(T):
+    # Extract rotation and translation
+    rotation_mat = T[0:3,0:3]
+    translation_mat = T[0:3,3]
+    # matrix to euler
+    euler = rot2euler(rotation_mat)
+    # get dummy handle
+    status, dummy_handle = vrep.simxGetObjectHandle(clientID, 'Dummy', vrep.simx_opmode_blocking)
+    if status!= vrep.simx_return_ok:
+    	raise Exception('Cannot get handle of dummy')
+    time.sleep(1)
+    # create dummy
+    status = vrep.simxSetObjectPosition(clientID,dummy_handle,-1,translation_mat,vrep.simx_opmode_blocking)
+    if status != vrep.simx_return_ok:
+    	raise Exception('Cannot get position of dummy')
+    status = vrep.simxSetObjectOrientation(clientID,dummy_handle,-1,euler,vrep.simx_opmode_blocking)
+    if status != vrep.simx_return_ok:
+    	raise Exception('Cannot get orientation of dummy')
+    time.sleep(1)
+
+# theoretical result
 def Forward_kinematics(deg1,deg2,deg3,deg4,deg5,deg6):
     # convert from deg to rad and wrap
     theta = np.array([Deg2rad(deg1),Deg2rad(deg2),Deg2rad(deg3),Deg2rad(deg4),Deg2rad(deg5),Deg2rad(deg6)])
@@ -34,11 +56,16 @@ def Forward_kinematics(deg1,deg2,deg3,deg4,deg5,deg6):
     T56 = T_mat(theta[5],d[5],a[5],alpha[5])
     # combine
     T = T01*T12*T23*T34*T45*T56
-    print(T)
-    print('mdzz')
+    # cut
+    T = T[0:3]
+    show_dummy(T)
+    # print
+    print("Theoretical result: ")
+    print(T[0:3])
     # return
     return T
 
+# experimental vritification
 def Move_to_joint_position(a0,a1,a2,a3,a4,a5):
     vrep.simxSetJointTargetPosition(clientID,joint_handle[0],Deg2rad(a0),vrep.simx_opmode_oneshot)
     vrep.simxSetJointTargetPosition(clientID,joint_handle[1],Deg2rad(a1),vrep.simx_opmode_oneshot)
@@ -46,41 +73,28 @@ def Move_to_joint_position(a0,a1,a2,a3,a4,a5):
     vrep.simxSetJointTargetPosition(clientID,joint_handle[3],Deg2rad(a3),vrep.simx_opmode_oneshot)
     vrep.simxSetJointTargetPosition(clientID,joint_handle[4],Deg2rad(a4),vrep.simx_opmode_oneshot)
     vrep.simxSetJointTargetPosition(clientID,joint_handle[5],Deg2rad(a5),vrep.simx_opmode_oneshot)
-    time.sleep(1)
-#def Get_pos_mat(joint_handle):
+    time.sleep(2)
+
+def Exp_veri(a0,a1,a2,a3,a4,a5):
+    print("Experimental result: ")
+    Move_to_joint_position(a0,a1,a2,a3,a4,a5)
+    _,pos_or_mat = vrep.simxGetJointMatrix(clientID,joint_handle[5],vrep.simx_opmode_blocking)
+    matrix = np.array(pos_or_mat).reshape((3,4))
+    print(matrix)
 
 # verify
-print("Zeroth experimental result: ")
-print(vrep.simxGetJointMatrix(clientID,joint_handle[5],vrep.simx_opmode_blocking))
-print("Zeroth theoretical result: ")
-Forward_kinematics(0,0,0,0,0,0)
-time.sleep(1)
-
-
-
-
-print("First theoretical result: ")
-Forward_kinematics(90,90,-90,90,90,90)
-print("First experimental result: ")
-Move_to_joint_position(90,90,-90,90,90,90)
-time.sleep(1)
-print(vrep.simxGetJointMatrix(clientID,joint_handle[5],vrep.simx_opmode_blocking))
-'''
-pos_or_mat = simxGetJointMatrix(clientID,joint_handle[5],vrep.simx_opmode_blocking)
-print(pos_or_mat)
-'''
-
-
-print("Second theoretical result: ")
 Forward_kinematics(-90,45,90,135,90,90)
-print("Second experimental result: ")
-Move_to_joint_position(-90,45,90,135,90,90)
+Exp_veri(-90,45,90,135,90,90)
 time.sleep(1)
-print(vrep.simxGetJointMatrix(clientID,joint_handle[5],vrep.simx_opmode_blocking))
-'''
-pos_or_mat = simxGetJointMatrix(clientID,joint_handle[5],vrep.simx_opmode_blocking)
-print(pos_or_mat)
-'''
+
+Move_to_joint_position(0,0,0,0,0,90)
+time.sleep(1)
+Move_to_joint_position(0,0,0,0,0,-90)
+time.sleep(1)
+Move_to_joint_position(0,0,0,0,0,90)
+time.sleep(1)
+Move_to_joint_position(0,0,0,0,0,-90)
+time.sleep(1)
 
 print('Done2')
 
--- code/helper_functions.py
+++ code/helper_functions.py
@@ -10,6 +10,25 @@ def Deg2rad(deg):
 def Rad2deg(rad):
     return rad * 180 / PI
 
+def rot2euler(rotation_mat):
+    print('mdzz')
+    if rotation_mat[0,2] < 1:
+        if rotation_mat[0,2] > -1:
+            beta = np.arcsin(rotation_mat[0,2])
+            alpha = np.arctan2(-rotation_mat[1,2],rotation_mat[2,2])
+            garma = np.arctan2(-rotation_mat[0,1],rotation_mat[0,0])
+        else:
+            beta = -PI/2
+            alpha = np.arctan2(rotation_mat[1,0],rotation_mat[1,1])
+            garma = 0
+    else:
+        beta = PI/2
+        alpha = np.arctan2(rotation_mat[1,0],rotation_mat[1,1])
+        garma = 0
+    # warpping
+    euler = np.array([alpha,beta,garma])
+    return euler
+
 def T_mat(theta,d,a,alpha):
     mat = np.matrix([[math.cos(theta), -math.sin(theta)*math.cos(alpha),  math.sin(theta)*math.sin(alpha),  a*math.cos(theta)],
                      [math.sin(theta),  math.cos(theta)*math.cos(alpha), -math.cos(theta)*math.sin(alpha),  a*math.sin(theta)],
@@ -17,4 +36,4 @@ def T_mat(theta,d,a,alpha):
                      [0              ,  0                              ,  0                              ,  1                ]])
     return mat
 
-def Arr2mat(arr):
+#def Arr2mat(arr):
